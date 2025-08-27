c0a979869a2e73bc12d004fb6ce5e98e196d6ae7
i-piiyush <piyushchhabra3578@gmail.com>
2025-08-28 02:56:38 +0530

    fixingcookies issue

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -61,21 +61,18 @@ const loginController = async (req, res) => {
     id: user._id,
   };
 
-  const token = jwt.sign(payload, process.env.JWT_SECRET);
-  res.cookie("token", token, {
-    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-    httpOnly: true,
-    secure: true, // only send over HTTPS
-    sameSite: "none", // required for cross-site cookies
-  });
-
-  isPasswordValid = await bcyrpt.compare(password, user.password);
+  const isPasswordValid = await bcrypt.compare(password, user.password);
   if (!isPasswordValid) {
-    return res.status(401).json({
-      message: "invalid password",
-    });
+    return res.status(401).json({ message: "invalid password" });
   }
 
+  const token = jwt.sign(payload, process.env.JWT_SECRET);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // local dev me false
+    sameSite: "none",
+    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+  });
   return res.status(200).json({
     message: "user succefully logged in",
     username,
